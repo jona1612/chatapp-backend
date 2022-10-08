b816cef64092a58a71b4693b72d2666847f6ff28
Jonathan <jonathan16official@gmail.com>
2022-10-08 17:51:05 +0530

    New

--- connection.js
+++ connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.s11qz.mongodb.net/chatAppMern?retryWrites=true&w=majority`, ()=> {
-//   console.log('connected to mongodb')
-// })
 
 // MongoDB Connection
 const URL = "mongodb+srv://john:Developer123@cluster0.hsmfp.mongodb.net/ChatApp?retryWrites=true&w=majority";
